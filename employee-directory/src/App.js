b2c7f1516e217de0cb6ebce46596389e83c37809
jenry valdes <jenryval93@gmail.com>
2021-03-28 18:06:14 -0500

    added axios

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import Wrapper from './Components/Wrapper'
+import Table from './Components/Table'
+import axios from 'axios'
+
+componentDidMount() {
+  console.log('hi')
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Wrapper>
+          <Table />
+        </Wrapper>
       </header>
     </div>
   );
 }
-
 export default App;
---
+++ employee-directory/src/Components/Employee/Employee.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Employee() {
+    return (
+        <div></div>
+    )
+}
+
+export default Employee;
